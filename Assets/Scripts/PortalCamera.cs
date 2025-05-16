49fb5d06945383988b806301da3f8abe5479c2a3
TGFo <tyriquenudle@outlook.com>
2025-05-16 14:53:07 +0200

    Initial commit

---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Camera portalCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
